8e12d858046a43057a86c7cb30ca7a375616ad75
Chnace625 <68520665+Chancedashie-GGGG@users.noreply.github.com>
2023-09-30 16:13:28 -0700

    Create picture.tsx

---
+++ components/picture.tsx
@@ -0,0 +1,16 @@
+
+interface PictureProps {
+    thumbnail: string | undefined;
+}
+
+const Thumbnail: React.FC<PictureProps> = ({
+    thumbnail
+}) => {
+    return (
+        <div className="bg-zinc-800 rounded-lg p-2 overflow-hidden w-36 aspect-square hover:scale-95 transition ease-in-out duration-300">
+            <img src={thumbnail} className="w-full h-full rounded-lg object-cover" />
+        </div>
+    );
+}
+
+export default Thumbnail;
