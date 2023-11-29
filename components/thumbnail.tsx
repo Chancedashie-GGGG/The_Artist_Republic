cc4df089dcfaa1bb8cd40415f17eeff15240c4ba
Chnace625 <68520665+Chancedashie-GGGG@users.noreply.github.com>
2023-11-29 06:14:12 -0800

    Update thumbnail.tsx

--- components/thumbnail.tsx
+++ components/thumbnail.tsx
@@ -2,16 +2,20 @@ import Button from "./button";
 interface ThumbnailProps {
     title: string;
     thumbnail: string | undefined;
+    link: string;
 }
 
 const Thumbnail: React.FC<ThumbnailProps> = ({
     title,
-    thumbnail
+    thumbnail,
+    link
 }) => {
     return (
         <div className="bg-zinc-800 rounded-lg p-5 overflow-hidden w-96 hover:scale-95 transition ease-in-out duration-300">
             <img src={thumbnail} className="w-96 rounded-lg aspect-video object-cover" />
-            <div className="pt-5"><div className="text-red-500 text-2xl font-semibold mb-2">{title}</div><Button label="Watch Now"></Button></div>
+            <div className="pt-5"><div className="text-red-500 text-2xl font-semibold mb-2">{title}</div>
+                <Button direct={link} label="WATCH VIDEO"></Button>
+            </div>
         </div>
     );
 }
