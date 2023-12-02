e9e4c45284301b3b71a8245e79ee60311bceb07b
Chnace625 <68520665+Chancedashie-GGGG@users.noreply.github.com>
2023-12-02 07:56:57 -0800

    I Need Help To Fix The Nav

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,20 +1,35 @@
+import { UserButton, useUser } from "@clerk/nextjs";
 import NavbarItem from "./navbaritem";
 
-const navbar = () => {
-    return (<div className="bg-zinc-950 text-red-500 p-3 flex justify-between items-center">
-        <img src="./img/Logowhite.png" className="w-36 mr-5" alt="The Artist Republic Logo" />
-        <div className="hidden lg:flex">
-                <NavbarItem label="Home"></NavbarItem>
-                <NavbarItem label="Drawings"></NavbarItem>
-                <NavbarItem label="Animations"></NavbarItem>
-                <NavbarItem label="Community"></NavbarItem>
+export function NavBar() {
+    const user = useUser();
+
+    return (
+        <div className="bg-zinc-950 text-red-500 w-full p-3 flex justify-between items-center">
+            <img src="/img/Logowhite.png" className="w-36 mr-5 hidden lg:block" alt="The Artist Republic Logo" />
+            <div className="flex lg:hidden">
+                <NavbarItem route="/" label="Browse"></NavbarItem>
+            </div>
+            <div className="hidden lg:flex">
+                <NavbarItem route="/" label="Home"></NavbarItem>
+                <NavbarItem route="" label="Drawings"></NavbarItem>
+                <NavbarItem route="" label="Videos"></NavbarItem>
+                <NavbarItem route="" label="Community"></NavbarItem>
             </div>
             <div className="flex flex-row ml-auto gap-3 items-center">
-                
-                <NavbarItem label="Sign In"></NavbarItem>
-                <NavbarItem label="Sign Up"></NavbarItem>
+                {user.isSignedIn && (
+                    <div>
+                        <NavbarItem route="/upload" label="Upload"></NavbarItem>
+                        <NavbarItem route="/feed" label="Feed"></NavbarItem>
+                    </div>
+                )};
+                {!user.isSignedIn && (
+                    <NavbarItem route="/sign-in" label="Sign In"></NavbarItem>
+                )}
+                <div>
+                    <UserButton afterSignOutUrl="/" />
+                </div>
             </div>
-    </div>);
+        </div>
+    );
 }
-
-export default navbar;
